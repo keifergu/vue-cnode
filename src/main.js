9daef1c69858f525a95faea5377e538742954c67
Keifer.Gu <keifergu@gmail.com>
2016-12-11 21:48:56 +0800

    添加iview；删除默认文件

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import VueRouter from 'vue-router'
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'
+
+Vue.use(VueRouter);
+Vue.use(iView);
 
 /* eslint-disable no-new */
 new Vue({
