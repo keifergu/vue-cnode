a18b8310736830d28cfe11ff500bd26d6e9e88bf
Keifer.Gu <keifergu@gmail.com>
2017-01-08 19:15:03 +0800

    初步完成用户信息界面

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,51 +1,83 @@
 <template>
-  <div>
+  <div class="container">
     <div v-if="loading">
       loading...
     </div>
     <div v-else>
-      <div>
-        {{user.loginname+" "+user.score}}
+      <mu-card>
+        <mu-card-header :title="user.loginname" :subTitle="user.score">
+          <mu-avatar :src="user.avatar_url" slot="avatar"/>
+        </mu-card-header>
+      </mu-card>
+      <div class="reply">
+        <mu-paper>
+          <mu-content-block v-for="reply in user.recent_replies">
+            <topic-list-item
+              :topicId="reply.id"
+              :top="reply.top"
+              :title="reply.title"
+              :author="reply.author"
+            />
+          </mu-content-block>
+        </mu-paper>
       </div>
-      <div v-for="reply in user.recent_replies">
-        {{reply.author.loginname+" "+reply.title}}
+
+      <div class="topic">
+        <mu-paper>
+          <mu-content-block v-for="topic in user.recent_topics">
+            <topic-list-item
+              :topicId="topic.id"
+              :top="topic.top"
+              :title="topic.title"
+              :author="topic.author"
+            />
+          </mu-content-block>
+        </mu-paper>
       </div>
-      <div v-for="topic in user.recent_topics">
-        {{topic.author.loginname+" "+topic.title}}
-      </div>
+
     </div>
   </div>
 </template>
 
 <script>
-import cnode from '../utils/api';
+  import cnode from '../utils/api';
+  import TopicListItem from './TopicListItem';
 
-export default {
-  name: 'user',
-  data() {
-    return {
-      user: '',
-      loading: true,
-    };
-  },
-  created() {
-    this.fetchUserInfo().then(data => {
-        console.log(data)
-        this.user = data;
-        this.loading = false;
-      })
-  },
-  methods: {
-    fetchUserInfo() {
-      let loginname = this.$route.params.loginname;
-      return cnode('user_info',{
-        pathParams: [loginname]
-      });
-    }
-  },
-};
+  export default {
+    name: 'user',
+    data() {
+      return {
+        user: '',
+        loading: true,
+      };
+    },
+    created() {
+      this.fetchUserInfo().then(data => {
+          console.log(data)
+          this.user = data;
+          this.loading = false;
+        })
+    },
+    methods: {
+      fetchUserInfo() {
+        let loginname = this.$route.params.loginname;
+        return cnode('user_info',{
+          pathParams: [loginname]
+        });
+      }
+    },
+    components: { TopicListItem }
+  };
 </script>
 
 <style scoped>
-
+  .container {
+    padding-top: 5px;
+  }
+  .reply {
+    margin-top: 5px;
+  }
+  .topic {
+    margin-top: 5px;
+  }
 </style>
