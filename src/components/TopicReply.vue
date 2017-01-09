0abfc69984fbb21414a9917e7c692e8ae8e75fb6
Keifer.Gu <keifergu@gmail.com>
2017-01-09 22:14:34 +0800

    添加回复组件

---
+++ src/components/TopicReply.vue
@@ -0,0 +1,29 @@
+<template>
+  <mu-paper class="container">
+    <div v-for="reply in replies">
+      <div class="avatar"></div>
+    </div>
+  </mu-paper>
+</template>
+
+<script>
+
+export default {
+  name: 'topic-reply',
+  props: {
+    replies: Array
+  },
+  computed: {
+
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style scoped>
+.container {
+
+}
+</style>
