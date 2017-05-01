e1b27bfa8e3c2c764591c39cf4c6f0afd82eccbc
Keifer.Gu <keifergu@gmail.com>
2017-05-01 20:15:18 +0800

    修改导航组件，添加图标

--- src/components/Navigator.vue
+++ src/components/Navigator.vue
@@ -1,21 +1,32 @@
 <template>
   <div>
-    <div class="avatar">
-      <mu-avatar :src="user.avatar_url" @click.native="linkTo('login')"/>
+    <div class="user">
+      <mu-list-item>
+        <mu-avatar :src="user.avatar_url" @click.native="linkTo('login')"/>
+      </mu-list-item>
+      <mu-list-item>
+        <div>点击登录</div>
+    </mu-list-item>
     </div>
+    <mu-divider />
     <mu-list-item @click="linkTo('home')">
+      <mu-icon slot="left" value="home"/>
       <router-link to="/home">首页</router-link>
     </mu-list-item>
     <mu-list-item @click="linkTo('essence')">
+      <mu-icon slot="left" value="thumb_up"/>
       <router-link to="/essence">精华</router-link>
     </mu-list-item>
     <mu-list-item @click="linkTo('question')">
+      <mu-icon slot="left" value="question_answer"/>
       <router-link to="/question">问答</router-link>
     </mu-list-item>
     <mu-list-item @click="linkTo('share')">
+      <mu-icon slot="left" value="supervisor_account"/>
       <router-link to="/share">分享</router-link>
     </mu-list-item>
     <mu-list-item @click="linkTo('jobs')">
+      <mu-icon slot="left" value="work"/>
       <router-link to="/jobs">工作</router-link>
     </mu-list-item>
   </div>
